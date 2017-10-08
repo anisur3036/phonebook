43f23a65dbe30cef8a18de348def5361049b9d50
Anisur Rahman <anisurlist3@gmail.com>
2017-10-08 09:50:15 +0600

    Adding laravel csrf_token meta tag

--- resources/views/phonebook.blade.php
+++ resources/views/phonebook.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>Phonebook</title>
 	<link rel="stylesheet" href="{{ asset('css/app.css') }}">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
